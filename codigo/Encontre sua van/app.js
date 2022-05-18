43a6d7aca46cf70ef880522bdcb280a5175ade24
Caio Reis <caio@vexter.com.br>
2022-05-17 23:13:18 -0300

    tela de listagem

--- codigo/Encontre sua van/app.js
+++ codigo/Encontre sua van/app.js
@@ -4,91 +4,81 @@ var db_contatos_inicial = {
             "id": 1,
             "nome": "Leanne Graham",
             "cidade": "Belo Horizonte",
-            "categoria": "amigos",
+            "turno": "Manhã",
             "email": "Sincere@april.biz",
             "telefone": "1-770-736-8031",
-            "website": "hildegard.org"
         },
         {
             "id": 2,
             "nome": "Ervin Howell",
             "cidade": "Betim",
-            "categoria": "familia",
+            "turno": "Tarde",
             "email": "Shanna@melissa.tv",
             "telefone": "010-692-6593",
-            "website": "anastasia.net"
         },
         {
             "id": 3,
             "nome": "Clementine Bauch",
-            "cidade": "Rio de Janeiro",
-            "categoria": "trabalho",
+            "cidade": "Ibirité",
+            "turno": "Tarde",
             "email": "Nathan@yesenia.net",
             "telefone": "1-463-123-4447",
-            "website": "ramiro.info"
         },
         {
             "id": 4,
             "nome": "Patricia Lebsack",
             "cidade": "Betim",
-            "categoria": "trabalho",
+            "turno": "Manhã",
             "email": "Julianne.OConner@kory.org",
             "telefone": "493-170-9623 x156",
-            "website": "kale.biz"
         },
         {
             "id": 5,
             "nome": "Chelsey Dietrich",
-            "cidade": "São Paulo",
-            "categoria": "familia",
+            "cidade": "Nova Lima",
+            "turno": "Noite",
             "email": "Lucio_Hettinger@annie.ca",
             "telefone": "(254)954-1289",
-            "website": "demarco.info"
         },
         {
             "id": 6,
             "nome": "Mrs. Dennis Schulist",
-            "cidade": "Rio de Janeiro",
-            "categoria": "trabalho",
+            "cidade": "Ibirité",
+            "turno": "Manhã",
             "email": "Karley_Dach@jasper.info",
             "telefone": "1-477-935-8478",
-            "website": "ola.org"
         },
         {
             "id": 7,
             "nome": "Kurtis Weissnat",
             "cidade": "Belo Horizonte",
-            "categoria": "amigos",
+            "turno": "Noite",
             "email": "Telly.Hoeger@billy.biz",
             "telefone": "210.067.6132",
-            "website": "elvis.io"
         },
         {
             "id": 8,
             "nome": "Nicholas Runolfsdottir V",
             "cidade": "Belo Horizonte",
-            "categoria": "familia",
+            "turno": "Noite",
             "email": "Sherwood@rosamond.me",
             "telefone": "586.493.6943",
-            "website": "jacynthe.com"
         },
         {
             "id": 9,
             "nome": "Glenna Reichert",
             "cidade": "Betim",
-            "categoria": "amigos",
+            "turno": "Manhã",
             "email": "Chaim_McDermott@dana.io",
             "telefone": "(775)976-6794",
-            "website": "conrad.com"
         },
         {
             "id": 10,
             "nome": "Clementina DuBuque",
-            "cidade": "São Paulo",
-            "categoria": "amigos",
+            "cidade": "Nova Lima",
+            "turno": "Noite",
             "email": "Rey.Padberg@karina.biz",
             "telefone": "024-648-3804",
-            "website": "ambrose.net"
         }
     ]
 }
@@ -115,8 +105,7 @@ function insertContato(contato) {
         "email": contato.email,
         "telefone": contato.telefone,
         "cidade": contato.cidade,
-        "categoria": contato.categoria,
-        "website": contato.website
+        "turno": contato.turno,
     };
 
     // Insere o novo objeto no array
@@ -136,8 +125,7 @@ function updateContato(id, contato) {
         db.data[index].email = contato.email,
         db.data[index].telefone = contato.telefone,
         db.data[index].cidade = contato.cidade,
-        db.data[index].categoria = contato.categoria,
-        db.data[index].website = contato.website
+        db.data[index].turno = contato.turno,
 
     displayMessage("Contato alterado com sucesso");
 
