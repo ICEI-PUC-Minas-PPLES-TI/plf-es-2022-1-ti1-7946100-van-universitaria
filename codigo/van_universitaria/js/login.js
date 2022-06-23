4f19ecdf90fea8d285cf0bc3c7881c90f95d10fc
Caio Reis <caio@vexter.com.br>
2022-06-23 10:38:58 -0300

    fix cadastro

--- codigo/van_universitaria/js/login.js
+++ codigo/van_universitaria/js/login.js
@@ -6,7 +6,8 @@ var usuarioCorrente = {};
 
 function generateUUID() { 
     var d = new Date().getTime(); 
-    var d2 = (performance && performance.now && (performance.now() * 1000)) || 0; unsupported
+
+    var d2 = (performance && performance.now && (performance.now() * 1000)) || 0; 
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16; 
         if (d > 0) { 
@@ -51,7 +52,6 @@ function initLoginApp() {
 
 
 function loginUser(login, senha) {
-    initLoginApp();
     for (var i = 0; i < db_usuarios.usuarios.length; i++) {
         var usuario = db_usuarios.usuarios[i];
 
